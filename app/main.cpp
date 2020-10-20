c8e48e29a3a4b5664c7912943968216f79ef5906
kushagra7176 <kushagraagrawal425@gmail.com>
2020-10-19 20:26:22 -0700

    [main.cpp] Added code to test travis and cmake.

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <lib.hpp>
-
-int main()
-{
-    dummy();
+#include<opencv2/opencv.hpp>
+#include <opencv2/dnn.hpp>
+using namespace cv;
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "OpenCV version : " << CV_VERSION << std::endl;
     return 0;
 }
+
