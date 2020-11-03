8c2febd089a8e4a9a5783a435f302953cd9df815
kartik venkat <kartik.venkat86@gmail.com>
2020-11-03 18:26:32 -0500

    [task 2.9]Created requirements shell script [requirements.sh] Added shell script with yolo requirements.

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+wget https://pjreddie.com/media/files/yolov3.weights
+
+wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
+
+wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
