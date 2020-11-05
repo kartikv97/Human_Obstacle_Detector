9d5f5c5f0063ecb8bc12b268605ee3f1db06fa5e
kartikv97 <56460840+kartikv97@users.noreply.github.com>
2020-11-05 04:01:52 -0500

    [task 2.9]  updated requirements.sh [requirements.sh] Updated requirements

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,7 @@
-wget https://pjreddie.com/media/files/yolov3.weights
+
 
 wget https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names
 
-wget https://github.com/pjreddie/darknet/blob/master/cfg/yolov3.cfg
+wget https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4.cfg
+
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v3_optimal/yolov4.weights
